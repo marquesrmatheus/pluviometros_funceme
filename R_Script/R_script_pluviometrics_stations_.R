d1820ac5126f28dcb0cf4379243c50b523e54387
Matheus Marques <marquesrmatheus@gmail.com>
2020-05-30 16:34:38 -0300

    Update R_script_pluviometrics_stations_.R

--- R_Script/R_script_pluviometrics_stations_.R
+++ R_Script/R_script_pluviometrics_stations_.R
@@ -1,22 +1,15 @@
 #' ---
-#' title: how to read csv files in r. Example: Pluviometric FUNCEME stations
-#' author: matheus marques
-#' date: 2020-05-29
+#' title: Working with .txt files from Pluviometric FUNCEME stations in R software. Example: 
+#' author: JosÃ© Matheus da Rocha Marques (@marquesrmatheus)
+#' last update: 2020-05-29
 #' ---
 
 rm(list=ls())
 
-# preparate r -------------------------------------------------------------
-# read packages
-#install.packages("magrittr")
-
+#' read packages
 library(readr)
 library(readxl)
 library(dplyr)
-library(animation)
-library(gganimate)
-library(ggplot2)
-library(ggimage)
 library(devtools)
 library(data.table)
 library(lubridate)
@@ -31,7 +24,7 @@ getwd()
 
 
 #' reading tables in r
-p734 <- read.table("734.txt",
+p734 <- read.table("734.txt", #.txt file of station number
                  header = T,
                  sep = ";",
                  stringsAsFactors = FALSE,
@@ -46,10 +39,10 @@ p734[is.na(p734)] = 0
 glimpse(p734)
 
 #'fixing characters Municipios column
-p734$Municipios = str_replace(p734$Municipios,"IndependÃªncia","Independência")
+p734$Municipios = str_replace(p734$Municipios,"IndependÃƒÂªncia","IndependÃªncia")
 glimpse(p734)
 
-#' Verificando a frequencia dos anos. Todos precisam estar com 12 para avaliação anual completa (12 meses)
+#' Verify years frequency. All years need to be with 12 observations (months)
 freq_years <- table(p734$Anos)
 freq_years
 
@@ -66,90 +59,18 @@ glimpse(df2)
 # create variable with list of years that have exactly 12 rows
 v.anos <- df2$Anos
 
-#
-sel.anos <- c(2009,2012,2013,2015,2015,2020)
+#sel.anos <- c(2009,2012,2013,2015,2015,2020)
 
 #Create a dataframe with list only years that have exactly 12 rows for each year
 p734 %>% 
   filter(Anos %in% v.anos)
 p734
+glimpse(p734)
 
-#Your dataframe already ok!!!
-
-glimpse()
-
-  filter(p734) <- doze_anos == TRUE
-
-  
-# filtering
-freq_anos <- table(p734$Anos)
-
-freq_anos
-
-doze_anos <- table(p734$Anos) == 12
-
-v.anos <- filter(doze_anos == TRUE)
-doze_anos <- freq_anos == 12
-
-new_var <- doze_anos["TRUE"]
-new_var
-
-
-  
-filter(function(p734) p734, doze_anos = TRUE)
-
-p1 <- p734[,doze_anos=TRUE]
-
-glimpse(p734) %>% 
-  filter(doze_anos==TRUE)
-
-p734 %>% 
-  filter(doze_anos==TRUE)
-p734
-
-for (doze_anos in p734$Anos) {
-}
-
-doze_anos <- table(p734$Anos) == 12
-
-
-filter(p734,doze_anos==TRUE)
-
-table(p734)[count(p734$Anos)=12]
-
-select(p734,Anos,table(Anos=12))
-
-
-
-new.ind.data <- p734 %>%
-  group_by(Anos) %>%
-  filter(n() = 12) %>%
-  ungroup()
-
-%>% 
-  filter(table(Anos) >= 12)
-  
-
-filter(p734,table(Anos) = 12))
-
-#' Verificando a frequencia dos anos. Todos precisam estar com 12 para avaliação anual completa (12 meses)
-table(p734$Anos)
-
-select(p734)
+#Your dataframe already ok for while!
+----------------------------------------------------------------------------------------------------
 
 colSums(data$Total)
-#' Verificando a frequÃªncia dos anos. Todos precisam estar com 12 para avaliaÃ§Ã£o anual completa (12 meses)
-table(p734$Anos)
-
-#' Deleting only rows that contains the year 2020 
-p1 <- p1[p1$Anos!="2020",]
-p1 <- p1[p1$Anos!=2019, ]
-table(p1$Anos)
-
-#' deleting rows that contains two or more years
-table(p1$Anos)
-p1 <- p1[ !(p1$Anos %in% c("2020","2019")), ]
-table(p1$Anos)
 
 summary.data.frame(p1)
 sum(p1$Total)
@@ -163,27 +84,3 @@ qplot(Anos,Total,data=p1,
   theme_bw()
 anosp1 <- colSums(p1$Total)
 Error
-# keeping columns that you would like to keep
-#keepcols <- c("ID","Anos", "Latitude", "Longitude")
-#p1 <- p1[ , keepcols ]
-
-# to remove just one column
-#p1[ , "Meses" ] <- NULL
-#p1
-
-# to remove two or more columns. You need to set a combine in a object.
-#colstodelete <- c("Latitude", "Longitude", "Meses")
-
-#and write these lines
-#names(p1) %in% colstodelete
-#!(names(p1) %in% colstodelete)
-#p1 <- p1[, !(names(p1) %in% colstodelete)]
-#p1
-
-# to remove two or more lines. You need to set a combine in a object.
-rowstodelete <- c("2020","2019")
-names(p1) %in% rowstodelete
-!(names(p1) %in% rowstodelete)
-p1 <- ~p1[, !(names(p1) %in% rowstodelete)]
-p1
-p1$Anos
